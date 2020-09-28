89ccc97d190754ba75349d8e170cc757e3f867be
dirtyjesuss <63318592+dirtyjesuss@users.noreply.github.com>
2020-09-28 19:54:28 +0300

    empty proj

---
+++ Homework3/Homework3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework3
+//
+//  Created by Ruslan Khanov on 28.09.2020.
+//  Copyright © 2020 Ruslan Khanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
